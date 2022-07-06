77a202142eb0145c0386cf6355de17b38ad789cf
wally2222 <82655614+wally2222@users.noreply.github.com>
2022-07-06 16:50:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("name.txt", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
+
+        
+      });
+  }
+);
 
